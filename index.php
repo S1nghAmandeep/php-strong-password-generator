00a07cfcd62e1861afdac742da59b529dd0d370b
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-23 16:00:48 +0100

    include function file/printed in dom

--- index.php
+++ index.php
@@ -1,40 +1,14 @@
 <?php
 
-
 $get_max_num = (int)$_GET['number'] ?? '';
 // var_dump($get_max_num);
 
 $random_letter = 'abcdefghijklmnopqrstuvwxyzç@ò#à!ì0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // var_dump($random_letter[10]);
 
-
-function generate_pass($lenght, $letters)
-{
-    if ($lenght > 7) {
-        $letters_lenght = strlen($letters);
-        $password = '';
-        for ($i = 0; $i < $lenght; $i++) {
-            $password .= $letters[random_int(0, $letters_lenght - 1)];
-        }
-        var_dump($password);
-
-        return $password;
-    } else {
-        return 'inserire il numero maggiore di 7';
-    }
-}
-
-echo generate_pass($get_max_num, $random_letter);
-
-
-// echo generate_pass();
+include 'functions.php';
 
 ?>
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -50,6 +24,8 @@ echo generate_pass($get_max_num, $random_letter);
             <input type="number" name="number" id="number" placeholder="Inseriesci il numero maggiore di 7">
             <button>Invia</button>
         </form>
+
+        <h1>La tua password è: <?php echo $password ?></h1>
     </main>
 </body>
 
